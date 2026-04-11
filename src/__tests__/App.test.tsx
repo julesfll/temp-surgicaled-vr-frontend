de107e9e41fa27fb81570185114c396946d2e0c6
julesfll <juleslemenestrel@gmail.com>
2026-04-11 14:09:51 -0400

    fix: add dummy tests

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+
+describe("App", () => {
+  it("should pass", () => {
+    expect(true).toBe(true);
+  });
+});
